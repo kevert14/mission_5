41f7d544cb6e10e79b7bce03c2e5cffc83d45655
kevert14 <kevert14@byu.edu>
2022-02-02 11:09:15 -0700

    first commit

---
+++ Migrations/20220202044111_Initial.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mission4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Year = table.Column<ushort>(nullable: false),
+                    Director = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    Edited = table.Column<bool>(nullable: false),
+                    Lent = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    CategoryID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieID);
+                    table.ForeignKey(
+                        name: "FK_Responses_Category_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Category",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "CategoryID", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, 1, "Peter Jackson", false, "", "", "PG-13", "Lord of the Rings: The Fellowship of the Ring", (ushort)2001 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "CategoryID", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, 1, "Peter Jackson", false, "", "", "PG-13", "Lord of the Rings: The Two Towers", (ushort)2002 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "CategoryID", "Director", "Edited", "Lent", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, 1, "Peter Jackson", false, "", "", "PG-13", "Lord of the Rings: The Return of the King", (ushort)2002 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Responses_CategoryID",
+                table: "Responses",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
